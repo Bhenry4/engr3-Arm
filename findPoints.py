f4ef3d4c9f1eb5ece289baff17a587eab1ecf883
Bhenry4 <82593996+Bhenry4@users.noreply.github.com>
2024-06-05 19:07:00 -0400

    Added comments

--- findPoints.py
+++ findPoints.py
@@ -1,4 +1,4 @@
-import runMotor
+import runMotor # motor logic in other file
 
 totalXSteps = 0
 totalYSteps = 0
@@ -6,16 +6,16 @@ totalYSteps = 0
 def f(x):
         return x**2
 
-def findPoints(r, f):
+def findPoints(r, f): # finds the values of points in a given range (interpeted as xx.x) with the given function
     points = []
     for x in r:
         try:
             points.append((x*0.1, f(x*0.1)))
         except:
-            pass
+            pass # returns nothing for divide by zero, etc. (discontinuities)
     return points
 
-def autoFit(points):
+def autoFit(points): # finds the minimum and maximum y-values for the given x-range (using the points list)
     ymax = 0
     ymin = 0
     for point in points:
@@ -25,12 +25,13 @@ def autoFit(points):
             ymin = point[1]
     return ymax, ymin
 
-def graphPoint(Xunits, Yunits, stepsPerXUnit, stepsPerYUnit, penDown):
+def graphPoint(Xunits, Yunits, stepsPerXUnit, stepsPerYUnit, penDown): # Takes a point in graph units and converts it to motor steps, then calls the motor module to move the motors
     Xsteps = Xunits * stepsPerXUnit
     Ysteps = Yunits * stepsPerYUnit
     runMotor.axes.moveTo(Xsteps, Ysteps)
     runMotor.raiseLowerPen(penDown)
 
+# this block uses the functions to find the points, figure out the graph unit to motor step conversion, then graph it
 points = findPoints(range(-100, 100+1), f)
 print(points)
 ymax, ymin = autoFit(points)
