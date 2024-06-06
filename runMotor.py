7f0000efd04533964373305b7c5b3de5ab379334
Bhenry4 <82593996+Bhenry4@users.noreply.github.com>
2024-06-05 20:08:01 -0400

    Added comments

--- runMotor.py
+++ runMotor.py
@@ -7,23 +7,23 @@ class MotorAxes:
     motorX: stepper.StepperMotor = None
     motorY: stepper.StepperMotor = None
     currPos: tuple[float] = None
-    def __init__(self, coilsX, coilsY, currPos=(0,0)):
+    def __init__(self, coilsX, coilsY, currPos=(0,0)): # currPos is an option in case you need to initialise w/o the axes being homed, but defaults to (0,0)
         self.motorX = stepper.StepperMotor(*coilsX, microsteps=None)
         self.motorY = stepper.StepperMotor(*coilsY, microsteps=None)
         self.currPos = currPos
     
-    def moveDistance(self, Xdist, Ydist):
+    def moveDistance(self, Xdist, Ydist): # moves a distance by looping onestep (best method I could find)
         for i in range(Xdist):
             self.motorX.onestep()
         for i in range(Ydist):
             self.motorY.onestep()
 
-    def moveTo(self, X, Y):
+    def moveTo(self, X, Y): # calculates distance to move
         Xdist = X - self.currPos[0]
         Ydist = Y - self.currPos[1]
         self.moveDistance(Xdist, Ydist)
 
-
+# H-Bridge pins
 coilsX = (
     digitalio.DigitalInOut(board.D9),
     digitalio.DigitalInOut(board.D10),
@@ -38,14 +38,14 @@ coilsY = (
     digitalio.DigitalInOut(board.D7)
 )
 
-for coil in coilsX, coilsY:
+for coil in coilsX, coilsY: # init coils and motor object
     coil.direction = digitalio.Direction.OUTPUT
 axes = MotorAxes(coilsX, coilsY)
 
-servoPin = pwmio.PWMOut(board.D2)
+servoPin = pwmio.PWMOut(board.D2) # init servo
 penServo = servo.Servo(servoPin)
 
-def raiseLowerPen(penDown):
+def raiseLowerPen(penDown): # called with argument from graphPoint
     if penDown == True:
         penServo.angle = 0
     else:
